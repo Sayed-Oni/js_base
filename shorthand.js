3adb63df7be37821538c431e7bd875794dbd0b83
Sayed-Oni1 <onihassan44@gmail.com>
2022-10-02 22:33:00 +0600

    -

---
+++ shorthand.js
@@ -0,0 +1,43 @@
+// variable declaration
+var tomatoPrice = 25;
+// variable update
+tomatoPrice = 30;
+
+// add something to the previous value
+var price1 = 31;
+var price2 = 20;
+
+// set new value
+// price1 = 35;
+
+// add new value to existing value
+price1 = price1 + 10;
+console.log(price1);
+price2 = price2 - 5;
+// console.log(price1);
+// console.log(price2);
+
+
+// addition to the same variable shorthand
+// += means Addition some value to the existing value
+// -= means Subtraction some value to the existing value
+// *= means Multiplication  some value to the existing value
+// /= means Division some value to the existing value
+price1 += 10;
+console.log(price1);
+price1 *= 5;
+console.log(price1);
+price1 /= 5;
+console.log(price1);
+price2 -= 5;
+console.log(price2);
+
+var age = 15;
+age = age + 1;
+// ++ means adding 1 to the existing
+age++;
+
+var love = 100;
+love = love - 1;
+// ++ means deducting 1 to the existing
+love--;
