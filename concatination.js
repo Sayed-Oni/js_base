048cf850584f56c44c1e231b20d909afe4cd3cca
Sayed-Oni1 <onihassan44@gmail.com>
2022-10-02 23:34:33 +0600

    -

---
+++ concatination.js
@@ -0,0 +1,13 @@
+var price1 = '31'; // string
+var price2 = 10; // string
+var price2 = "gg"; // number
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+// convert string to number
+console.log(price1);
+console.log(price2);
+// parseInt = parse Integer or convert a string to number
+price1 = parseInt(price1);
+// or
+var priceNumber = parseInt(price1);
+console.log(price1);
