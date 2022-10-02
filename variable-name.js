cbc441e4038893f0d9de137c948c22e6c8addb05
Sayed-Oni1 <onihassan44@gmail.com>
2022-10-02 21:14:12 +0600

    first commit

---
+++ variable-name.js
@@ -0,0 +1,23 @@
+var price = 29;
+
+// 1. variable name can not be any keywords or reserved word
+var false = 96;
+var return = true;
+
+// 2. variable name has to be in one word. no space
+var my home address = "new california"
+
+// 3. variable name can not have quotation
+var "name " = "tom hanks";
+
+// variable name can not starts with a number but can ends with a number
+var 99club = 1964
+
+// 5. how to use long names / cannot use dash
+var user-name = "raj bappa";
+var user home address = " andor killa bandor ban";
+
+var user_home_address = "andor killa bandor ban"; // snake case
+
+var userHomeAddress = "andor killa bandor ban"; // camel case
+var UserHomeAddress = "andor killa bandor ban"; // pascal case
---
+++ variable.js
@@ -0,0 +1,23 @@
+// vary + able = variable
+
+//  Numeric
+var price = 11;
+var age = 17;
+var temperature = 39;
+var weight = 78;
+var money = 450;
+var height = 5.9;
+
+// Sting
+var race = "white";
+var person = "samad vai";
+var person = "sodor uddin";
+var location = "andor killa bandorbon";
+var special = 'alia bhatt';
+
+// Boolean
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
+
+console.log(isSingle)
